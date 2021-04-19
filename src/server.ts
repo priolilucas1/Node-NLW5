95159b19e43f3c6dd96a131fe4017348e5db033a
priolilucas1 <priolilucas1@gmail.com>
2021-04-19 14:33:51 -0300

    🚀 Initialized project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Olá Mundo!" });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Usuário salvo com sucesso" });
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
