843f5b8a31f6afe65460d0e8db06097d40bc6c28
priolilucas1 <priolilucas1@gmail.com>
2021-04-20 13:40:46 -0300

    Settings repository created

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Settings } from "../entities/Settings";
+
+@EntityRepository(Settings)
+class SettingsRepository extends Repository<Settings> {}
+
+export { SettingsRepository };
