16226d310ff9683833c6d588b7437c9a1fe5417e
priolilucas1 <priolilucas1@gmail.com>
2021-04-22 20:26:36 -0300

    Find by email function created

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -26,6 +26,12 @@ class UsersService {
 
     return user;
   }
+
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({ email });
+
+    return user;
+  }
 }
 
 export { UsersService };
