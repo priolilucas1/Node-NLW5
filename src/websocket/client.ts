a72edcd70996fe90cc7e4e1b61ca906c8afa0923
priolilucas1 <priolilucas1@gmail.com>
2021-04-22 20:55:18 -0300

    Client connection authentication done

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,14 +1,22 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
+import { MessagesService } from "../services/MessagesService";
+
+interface ParamsProps {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionsService();
   const usersService = new UsersService();
+  const messagesService = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
-    const { text, email } = params;
+    const { text, email } = params as ParamsProps;
+    let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
 
@@ -18,11 +26,26 @@ io.on("connect", (socket) => {
         socket_id,
         user_id: user.id,
       });
+
+      user_id = user.id;
     } else {
-      await connectionsService.create({
-        socket_id,
-        user_id: userExists.id,
-      });
+      user_id = userExists.id;
+      const connection = await connectionsService.findUserById(userExists.id);
+
+      if (!connection) {
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id,
+        });
+      } else {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection);
+      }
     }
+
+    await messagesService.create({
+      text,
+      user_id,
+    });
   });
 });
