e28a131174b6e48f16dbf4be3d5f5bc87dbd2cb5
priolilucas1 <priolilucas1@gmail.com>
2021-04-22 20:28:08 -0300

    Websocket client created

---
+++ src/websocket/client.ts
@@ -0,0 +1,28 @@
+import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+import { UsersService } from "../services/UsersService";
+
+io.on("connect", (socket) => {
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+
+  socket.on("client_first_access", async (params) => {
+    const socket_id = socket.id;
+    const { text, email } = params;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create(email);
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id,
+      });
+    } else {
+      await connectionsService.create({
+        socket_id,
+        user_id: userExists.id,
+      });
+    }
+  });
+});
