a3346928e5967843e45af5a989df64ff91cc891e
priolilucas1 <priolilucas1@gmail.com>
2021-04-22 21:05:01 -0300

    Update Settings function created

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -40,6 +40,17 @@ class SettingsService {
 
     return settings;
   }
+
+  async update(username: string, chat: boolean) {
+    await this.settingsRepository
+      .createQueryBuilder()
+      .update(Settings)
+      .set({ chat })
+      .where("username = :username", {
+        username,
+      })
+      .execute();
+  }
 }
 
 export { SettingsService };
